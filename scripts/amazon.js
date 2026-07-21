6848f17a3df7bb169b5fc7f742cd1218e459d120
SURYA <suryach2006@gmail.com>
2026-07-21 21:25:38 +0530

    set the selector

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product)=>{
             </div>
 
             <div class="product-quantity-container">
-                <select>
+                <select class="js-quantity-selector-${product.id}">
                 <option selected value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -60,6 +60,10 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
     button.addEventListener('click',()=>{
         const productId = button.dataset.productId;
 
+        const selectButton=document.querySelector(`.js-quantity-selector-${productId}`);
+
+        const quantity = Number(selectButton.value);
+
         let matchingItem;
         cart.forEach((item)=>{
             if(productId===item.productId){
@@ -68,11 +72,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
         });
 
         if(matchingItem){
-            matchingItem.quantity+=1;
+            matchingItem.quantity+=quantity;
         }else{
             cart.push({
             productId: productId,
-            quantity: 1
+            quantity: quantity
         });
         }
 
